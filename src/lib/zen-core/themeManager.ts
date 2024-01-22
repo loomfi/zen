b6619c7186831b2f29206163765f2a20704d0a02
Defy <61808223+De-y@users.noreply.github.com>
2024-01-22 10:16:52 -0800

    ok

--- src/lib/zen-core/themeManager.ts
+++ src/lib/zen-core/themeManager.ts
@@ -1,2 +1,7 @@
-import { writable } from "svelte/store";
-import fetch from 'node-fetch';
+import { writable } from "svelte/store"
+import got from 'got'
+
+export async function currentTheme() {
+    const {data} = await got.get('https://httpbin.org/anything')
+    return '/addons/themes/light';
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import { currentTheme } from "$lib/zen-core/themeManager";
 </script>
 <head>
-    <link rel="stylesheet" href={currentTheme() + "/index.css"}>
+    <link rel="stylesheet" href={'/addons/themes/light' + "/index.css"}>
 </head>
 <main>
     <h1>avnce introduces Zen</h1>
