cc6b3e13454a54526efc6e84e7479ccc06357495
Defy <61808223+De-y@users.noreply.github.com>
2024-01-22 20:29:24 -0800

    cookie

--- src/lib/zen-core/themeManager.ts
+++ src/lib/zen-core/themeManager.ts
@@ -1,7 +1,10 @@
 import { writable } from "svelte/store"
 import axios from 'axios';
+import * as cookie from 'cookies'
 
-var theme = writable(await axios.get('/api/themes').then(function (response) {return response.data}).catch(function() {}))
+let c = new cookie.set('')
+let theme = writable('/addons/themes/dark')
+await axios.get('/api/themes').then(function (response) {theme.set(response.data)}).catch(function(error) {})
 export function currentTheme() {
     var themeN;
     theme.subscribe((value) => {
@@ -13,3 +16,4 @@ export function currentTheme() {
 export function updateTheme(themeName: string) {
     theme.set(themeName)
 }
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import {currentTheme} from '$lib/zen-core/themeManager'
+    import {currentTheme, updateTheme} from '$lib/zen-core/themeManager'
 </script>
 <head>
     <link rel="stylesheet" href={currentTheme() + "/index.css"}>
 </head>
-<h1>Welcome to avnce ZeN.</h1>
+<h1>Welcome to ZeN.</h1>
