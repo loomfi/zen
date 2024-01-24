19fcc52e81b5c105acd752ec72859a94e8856152
Defy <61808223+De-y@users.noreply.github.com>
2024-01-24 10:14:41 -0800

    css nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,16 +15,21 @@
     if (en == 'undefined/index.css') {
         en = '/addons/themes/light'
     }
+    var img = 'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg'
 </script>
 <head>
     <link rel="stylesheet" href={en}>
 </head>
-<div class="login">
-    <div id="loadOverlay" style="background-color:#333; position:absolute; top:0px; left:0px; width:100%; height:100%; z-index:2000;"></div>
-    <h1>Log in Zen</h1>
-    <form>
-        <input placeholder="Username" required>
-        <input placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
+<div id="loadOverlay" style="background-color:#333; position:absolute; top:0px; left:0px; width:100%; height:100%; z-index:2000;"></div>
+<div style={`background-image: url("${img}")`} id="login_img"></div>
+
+<div class="login-portal">
+    <div class="login">
+        <form class="login-form">
+            <h1 class="login-form-title">Zenith.</h1>
+            <input class="login-input" placeholder="Username" required>
+            <input placeholder="Password" class="login-input" required>
+            <button type="submit" class="login-button">Login</button>
+        </form>
+    </div>
 </div>
