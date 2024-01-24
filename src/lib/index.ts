b719a6a0ea60e067f4201e737bc7f0cdb7237374
Defy <61808223+De-y@users.noreply.github.com>
2024-01-23 18:47:07 -0800

    ok fixed

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 import './zen-core/cookieManager'
+import './zen-core/themeManager'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,20 @@
     import {cookie} from '$lib/zen-core/cookieManager'
 	import { onMount } from 'svelte';
     let f: String;
+    var en;
     onMount(() => {
         const e = new cookie()
         if (e.getCookie('theme') == '') {
             e.setCookie('theme', currentTheme(), ['30','y',false])
         }
         f = e.getCookie('theme')
+        en = f + "/index.css"
     });
+    if (en == 'undefined/index.css') {
+        en = '/addons/themes/light'
+    }
 </script>
 <head>
-    <link rel="stylesheet" href={f + "/index.css"}>
+    <link rel="stylesheet" href={en}>
 </head>
 <h1>Welcome to ZeN.</h1>
