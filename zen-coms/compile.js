61c644f6bb9602af6768c5b5d053947fd83ccb0e
Defy <61808223+De-y@users.noreply.github.com>
2024-01-25 09:46:28 -0800

    Make vercel compilation system

--- zen-coms/compile.js
+++ zen-coms/compile.js
@@ -2,8 +2,9 @@ import {exec} from "child_process"
 import * as fs from 'fs-extra'
 import * as d_pkg_m from 'detect-package-manager'
 import {platform} from "process"
-const package_manager = await d_pkg_m.detect()
 
+const isVercelBuild = process.argv.includes("--vercel");
+const package_manager = await d_pkg_m.detect()
 
 console.clear()
 console.log("Zen Compiler v1")
@@ -11,15 +12,26 @@ console.log(`©${new Date().getFullYear()} avnce. This project is open-source.\n
 console.log(`Your package manager is ${package_manager}`)
 console.log(`The platform that you are on is ${platform}`)
 console.log("You are currently building ZEN-BASE.\n\n")
-
+if (isVercelBuild == true) {
+    console.log("[*] Beginning Vercel Compilation.")
+    console.log("[*] Moving addons folder to the appropriate location.")
+    fs.copy('addons/','static/addons/', {recursive: true})    
+}
 if (package_manager == 'pnpm') {
     fs.remove('build/', (error) => {
         if (error) return console.error(error)
     });
     console.log("[*] Currently Building")
     await exec('pnpm run build', function() {
-        console.log("[*] Moving addons folder.")
-        fs.copy('addons/','build/client/addons', {recursive: true})    
+        if (isVercelBuild != true) {
+            console.log("[*] Moving addons folder.")
+            fs.copy('addons/','build/client/addons', {recursive: true})
+        } else {
+            console.log("[*] Deleting addons folder after finished with compilation.")
+            fs.remove('static/addons/',(error) => {
+                if (error) return console.error(error)
+            });
+        }    
         console.log('[*] Compilation done!')
         console.log('Welcome to Zen.')
     })
@@ -30,8 +42,14 @@ if (package_manager == 'npm') {
     });
     console.log("[*] Currently Building")
     await exec('npm run build', function() {
-        console.log("[*] Moving addons folder.")
-        fs.copy('addons/','build/client/addons', {recursive: true})    
-        console.log('[*] Compilation done!')
+        if (isVercelBuild != true) {
+            console.log("[*] Moving addons folder.")
+            fs.copy('addons/','build/client/addons', {recursive: true})
+        } else {
+            console.log("[*] Deleting addons folder after finished with compilation.")
+            fs.remove('static/addons/',(error) => {
+                if (error) return console.error(error)
+            });
+        }
     })
 }
